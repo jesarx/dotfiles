e6e112c22a90ac94a3053a9c45d5f3c3c869b19f
jesarx <jesarx@proton.me>
2026-07-04 08:10:17 -0600

    minimal tweaks

--- waybar/scripts/powermenu.sh
+++ waybar/scripts/powermenu.sh
@@ -4,15 +4,15 @@
 # ===================================================
 # Triggered by the custom/power module in the waybar config.
 
-lock="  Bloquear"
-reboot="  Reiniciar"
-shutdown="  Apagar"
+lock="  Lock"
+reboot="  Reboot"
+shutdown="  Shotdown"
 
-chosen=$(printf '%s\n' "$lock" "$reboot" "$shutdown" | \
-    wofi --dmenu --prompt "Sistema" --width 240 --height 200 --lines 3 --location center)
+chosen=$(printf '%s\n' "$lock" "$reboot" "$shutdown" |
+  wofi --dmenu --prompt "System" --width 240 --height 200 --lines 3 --location center)
 
 case "$chosen" in
-    "$lock")     swaylock ;;
-    "$reboot")   systemctl reboot ;;
-    "$shutdown") systemctl poweroff ;;
+"$lock") swaylock ;;
+"$reboot") systemctl reboot ;;
+"$shutdown") systemctl poweroff ;;
 esac
